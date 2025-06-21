a7a26cc2ce8c43648f55c3c9713a4f60699ddf15
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-21 13:24:56 -0500

    vamos suave

--- prueba.js
+++ prueba.js
@@ -1,1 +1,11 @@
 console.log("repasando git");
+let numeroUno = 10;
+let numeroDos = 20;
+
+if (numeroUno > numeroDos) {
+  console.log("condicion verdadera");
+} else console.log("condicion falsa");
+
+//git add.
+// git commit -m  "voy aqui"
+//git push origin main
