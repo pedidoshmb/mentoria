37fcc6dec4feee3d4bad71837cbc7c2ffeff9fd5
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-23 12:09:14 -0500

    calcular peso de jirafa - suma -

---
+++ ejercicio4.js
@@ -0,0 +1,10 @@
+/*
+Calcula el peso de una jirafa que epsa 1120kg y come 141kilos de hojas
+*/
+
+let jierafa = 1120;
+let comida = 141;
+
+let resultado = jierafa + comida;
+
+console.log("La jirafa pesa " + resultado + " kilogramos");
