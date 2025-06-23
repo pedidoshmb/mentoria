e0a94dd03693e4144148b1f344aefafadf7407fc
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-23 11:29:16 -0500

    calcular numero de galletas por cajas

---
+++ ejercicio2.js
@@ -0,0 +1,13 @@
+/*
+
+Tenemos 7 cajas con 8 cookies rellenas de chocolate cada una 
+
+cuantas galletas tenemos en total
+*/
+
+let numeroCajas = 7;
+const cantidadGalletas = 8;
+
+let resultado = numeroCajas * cantidadGalletas;
+
+console.log("La cantidad de galletas que tenemos es de :" + resultado);
