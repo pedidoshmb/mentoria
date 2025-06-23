e573b56db94ffe8f902ea08ced6a8a4b060cd0ce
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-23 14:32:33 -0500

    Empezando a ver condicionales if, else y else if

---
+++ condicionales/elseif.js
@@ -0,0 +1,20 @@
+/*
+
+vamos a pedir hamburguesa sin cebolla o con cebolla, 
+
+*/
+let deseaCebolla = false;
+
+if (!deseaCebolla) {
+  console.log("hamburguesa sin cebolla");
+} else console.log("Hamburguesa con cebolla");
+
+/*
+Tenemos un alumno joaquin y tiene edad de 31 años, quiero comprobar si es mayor de edad para inscribirse al curso */
+
+let edad = 10;
+let nombre = "Mauricio Barrios";
+
+if (edad >= 18) {
+  console.log(`${nombre} : se puede insbribir al curso`);
+} else console.log(`Debes esperar ${nombre} a ser mayor de edad`);
---
+++ condicionales/main.js
@@ -0,0 +1,7 @@
+//CONDICIONALES IF
+let estaLloviendo = false;
+
+if (estaLloviendo === true) {
+  // Ejecuta
+  console.log("llevar  sombrilla");
+} else console.log("lleve ventilador");
