21a620f6013aade0437b1653af269c441532a9d2
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-23 11:14:41 -0500

    creamos ejercicio cuanto tiempo le tomaria a una nave llegar a la luna

---
+++ ejercicio1.js
@@ -0,0 +1,13 @@
+/*
+
+Calcula cuantas horas le llevaria llegar a  la luna a una nave espacial y guarda el resultado en una
+variable.
+-La distancia desde la tierra a la Luna es de 384.000 km
+-La velocidad de la nave es de 1225Km x hora
+
+*/
+
+let distanciaLuna = 384000;
+let velocidadNave = 1225;
+let resultado = distanciaLuna / velocidadNave;
+console.log(`Las horas que le tomaria llegar son de: ${resultado}`);
