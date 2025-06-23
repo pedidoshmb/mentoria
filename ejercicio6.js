15680c4d6cd644f16f0770ac343ac778210adae6
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-23 13:51:46 -0500

    valor a pagar con descuento

---
+++ ejercicio6.js
@@ -0,0 +1,12 @@
+/*
+Oferta en tienda de videoJuegos
+si compras un jeugo de 50 Euros o mas te hacemos descuento de 20%
+
+si un cliente compra el fifa2025 que vale 50Euros cuanto deberia pagar el cliente
+
+*/
+let valorDelJuego = 60;
+let descuento = valorDelJuego * 0.2;
+let totalPagar = valorDelJuego - descuento;
+
+console.log(`El precio a pagar es de: ${totalPagar}`);
