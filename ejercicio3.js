78a0f82ccecb0a82fa9a792863207f62752fd436
Mauricio Barrios <pedidoshmb@gmail.com>
2025-06-23 12:03:41 -0500

    Cuantos minutos tienen 7 horas

---
+++ ejercicio3.js
@@ -0,0 +1,13 @@
+/*
+Cuantos minutos tiene 7 horas, se deben usar variables y operadores apra resolverlo 
+
+*/
+
+const minutosPorHora = 60;
+let numeroHoras = 7;
+
+let resultado = minutosPorHora * numeroHoras;
+
+console.log(
+  "las " + numeroHoras + " Horas tienen un total de " + resultado + " Minutos"
+);
